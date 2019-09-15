2f52994c11738c859e0eb40adcc919b3b095af90
RUOYAO YANG <yangruoyao@wustl.edu>
2019-09-14 23:28:42 -0500

    BFS

--- search.py
+++ search.py
@@ -102,7 +102,7 @@ def depthFirstSearch(problem):
     start_node = problem.getStartState()
     process_stack.push(start_node)
     
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    #print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     end_node = False
     #if the successor fringe node is not the goal state, keep searching
     while not process_stack.isEmpty():
@@ -115,8 +115,8 @@ def depthFirstSearch(problem):
             continue
         if problem.isGoalState(current_node[0]):
             print("success")
-            print len(node_visited)
-            print len(action_taken)
+            #print len(node_visited)
+            #print len(action_taken)
             action_taken.append(current_node[1])
             # exclude action_taken[0] for the start node
             action_taken = action_taken[1:]
@@ -125,7 +125,7 @@ def depthFirstSearch(problem):
 
         ##This part will take care of the end note situation
         if end_node: 
-            print(current_node[0])
+            #print(current_node[0])
             
             last_branch_node = last_branching_node_arr.pop()
             distance = abs(last_branch_node[0]-current_node[0][0])+abs(last_branch_node[1]-current_node[0][1])
@@ -133,10 +133,9 @@ def depthFirstSearch(problem):
             while distance >1:
                 last_branch_node = last_branching_node_arr.pop()
                 distance = abs(last_branch_node[0]-current_node[0][0])+abs(last_branch_node[1]-current_node[0][1])
-            print last_branch_node
+            #print last_branch_node
 
             node_index = node_visited.index(last_branch_node)
-            #print node_visited[:node_index+1]
             node_processed.extend(node_visited[node_index+1:])
             node_visited = node_visited[:node_index+1]
             action_taken = action_taken[:node_index+1]
@@ -155,7 +154,7 @@ def depthFirstSearch(problem):
                 target -=1
 
         if target == 0 :
-            print "This is end node*******************************"
+            #print "This is end node*******************************"
             end_node = True
         # add last_branching_node for (successors haven't been visited -1) to retreat
         elif target ==2:
@@ -169,14 +168,13 @@ def depthFirstSearch(problem):
             #push the node to the to-visit stack
             process_stack.push(fringe_node)
           
-            print "fringe node is:" ,fringe_node
+            #print "fringe node is:" ,fringe_node
         
-        print("here wait a sec")
+        #print("here wait a sec")
             
     print("Fail")
-            
-
-    #util.raiseNotDefined()
+    return action_taken[1:]
+    util.raiseNotDefined()
 
 
 
@@ -186,7 +184,43 @@ def breadthFirstSearch(problem):
     [2nd Edition: p 73, 3rd Edition: p 82]
     """
     "*** YOUR CODE HERE ***"
+    # nodes to be processed, format: (location, action, action cost)
+    process_queue = util.Queue()
+
+    # nodes visited, store location -(x,y)
+    node_visited = []
+
+    # get start node and push it onto the stack
+    start_node = problem.getStartState()
+    # create a list[] for each path
+    process_queue.push((start_node,[]))
+
+    # if the successor fringe node is not the goal state, keep searching
+    while not process_queue.isEmpty():
+
+        current_node = process_queue.pop()
+        if current_node[0] in node_visited:
+            continue
+        if problem.isGoalState(current_node[0]):
+            print("success")
+            # action is the path saved in current node
+            return current_node[1]
+
+        node_visited.append(current_node[0])
+        successors = problem.getSuccessors(current_node[0])
+        # restore the path from the start to current node
+        parent_path = current_node[1]
+        for fringe_node in successors:
+            location = fringe_node[0]
+            # connect the path from current node to each fringe node
+            fringe_path = parent_path + [fringe_node[1]]
+            # push the node to the to-visit stack
+            process_queue.push((location, fringe_path))
+
+    print("Fail")
     util.raiseNotDefined()
+
+
 
 def uniformCostSearch(problem):
     "Search the node of least total cost first. "
